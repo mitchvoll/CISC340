d537d1dec41fb54593fc60d024dc4440af171eef
Scott Killeen <scott.killeen@hotmail.com>
2015-10-12 22:34:34 -0400

    Led class

--- _main_/led.cpp
+++ _main_/led.cpp
@@ -1,3 +1,7 @@
+#include <Adafruit_MotorShield.h>
+
+#include <Adafruit_NeoPixel.h>
+
 
 
 #include "Arduino.h"
@@ -26,11 +30,18 @@ void Led::setAll(int onOff,int rgb[]){
 
 
 void Led::setLed(int led_1_to_16, int onOff, int rgb[]){
-
+    if(onOff) pixels.setPixelColor( led_1_to_16, pixels.Color(rgb[0],rgb[1],rgb[2]));
+    else pixels.setPixelColor(led_1_to_16, pixels.Color(0,0,0));
+    pixels.show();
   
 }
 
 void Led::setRange(int led_1_to_16_start, int led_1_to_16_end, int onOff, int rgb[]){
+    for(int i=led_1_to_16_start;i<=led_1_to_16_start;i++){
+      if(onOff) pixels.setPixelColor(i, pixels.Color(rgb[0],rgb[1],rgb[2]));
+      else pixels.setPixelColor(i, pixels.Color(0,0,0));
+      pixels.show();
+    }
 
   
 }
